bb8c149ed548d97caf0692119bc1b3681fa6fa9e
nikki35944 <95740936+nikki35944@users.noreply.github.com>
2023-08-15 23:05:25 +0300

    show category page

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -6,12 +6,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Категории</h1>
+                        <h1 class="m-0">Категория - {{ $category->title }}</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Категории</li>
+                            <li class="breadcrumb-item active">Категория</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -25,7 +25,6 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-1">
-                        <a href="{{ route('admin.category.create') }}" type="button" class="btn btn-block btn-primary">Добавить</a>
                     </div>
                 </div>
                 <div class="row">
@@ -33,22 +32,15 @@
                         <div class="card">
                             <div class="card-body table-responsive p-0">
                                 <table class="table table-hover text-nowrap">
-                                    <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Название</th>
-                                        <th>Действие</th>
-
-                                    </tr>
-                                    </thead>
                                     <tbody>
-                                    @foreach($categories as $category)
                                     <tr>
+                                        <td>ID</td>
                                         <td>{{ $category->id }}</td>
-                                        <td>{{ $category->title }}</td>
-                                        <td><a href="{{ route('admin.category.show', $category->id) }}"><i class="fas fa-regular fa-eye"></i></a></td>
                                     </tr>
-                                    @endforeach
+                                    <tr>
+                                        <td>Название</td>
+                                        <td>{{ $category->title }}</td>
+                                    </tr>
                                     </tbody>
                                 </table>
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,6 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::get('/', 'IndexController')->name('admin.category.index');
         Route::get('/create', 'CreateController')->name('admin.category.create');
         Route::post('/', 'StoreController')->name('admin.category.store');
+        Route::get('/{category}', 'ShowController')->name('admin.category.show');
     });
 });
